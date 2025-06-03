c167fe3738a058c3f38914313115332b21923a13
raineebhangre <raineebhangre45@gmail.com>
2025-06-03 12:39:55 +0530

    post creation and viewing

--- index.js
+++ index.js
@@ -5,9 +5,23 @@ const app=express();
 const port=3000;
 
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.get("/",(req,res)=>{
     res.render("index.ejs");
+});
+
+app.get("/new",(req,res)=>{
+    res.render("new.ejs");
+});
+
+let article=[];
+
+app.post("/submit",(req,res)=>{
+    article.push(req.body["title"]);
+    res.render("index.ejs",{
+        article1:article
+    })
 });
 
 app.listen(port,()=>{
