16419c693cf35bc6b1157c8ae16c9b2264c792af
raineebhangre <raineebhangre45@gmail.com>
2025-06-04 11:53:14 +0530

    post editing

--- index.js
+++ index.js
@@ -51,10 +51,37 @@ app.post('/delete/:title', (req, res) => {
     if(article[i].title === requestedTitle){
         article = article.filter((item) => item.title !== requestedTitle);
     }
-    break;
   }
   res.redirect("/");
 });
+
+app.get("/edit/:title",(req,res)=>{
+    const requestedTitle=req.params.title;
+
+    for(var i=0;i<article.length;i++){
+        if(article[i].title === requestedTitle){
+            res.render("edit.ejs",{
+                title: article[i].title,
+                content: article[i].content
+            });
+        }
+    }
+});
+
+app.post('/edit/:title', (req, res) => {
+    const requestedTitle = req.params.title;
+    const title1= req.body["title"];
+    const content1= req.body["content"];
+
+    for(var i=0;i<article.length;i++){
+        if(article[i].title === requestedTitle){
+            article[i].title=title1;
+            article[i].content=content1;
+            break;
+        }
+    }
+    res.redirect("/");
+})
 
 app.listen(port,()=>{
     console.log(`Listening on port ${port}`);
